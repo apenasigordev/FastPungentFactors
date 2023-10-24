82e0601ad949d9bf5b6284fade9710df5e8402bb
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2023-10-23 21:35:08 -0300

    Update script.mjs

--- script.mjs
+++ script.mjs
@@ -16,13 +16,13 @@ const camera = new THREE.PerspectiveCamera(
 // The renderer: something that draws 3D objects onto the canvas
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x0000aa, 1);
+renderer.setClearColor(0x001466, 1);
 // Append the renderer canvas into <body>
 document.body.appendChild(renderer.domElement);
 
 const texture = new THREE.TextureLoader().load('textures/floosh.png' ); 
 
-const geometry = new THREE.SphereGeometry( 1, 64, 32 );
+const geometry = new THREE.SphereGeometry( 1, 32, 16 );
 
 const material = new THREE.MeshBasicMaterial( { map: texture } );
 
@@ -30,12 +30,12 @@ const sphere = new THREE.Mesh( geometry, material );
 
 scene.add( sphere );
 
-camera.position.z = 5
+camera.position.z = 8
 
 function render() {
 	renderer.render(scene, camera);
         // sphere.rotation.x += 0.05;
-        sphere.rotation.y -= 0.08;
+        sphere.rotation.y -= 0.05;
 	requestAnimationFrame(render);
 }
 
